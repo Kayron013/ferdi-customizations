5e5c641018e0840d9283af42a0d4ee9b1b8fd40a
Angel <kayron013@gmail.com>
2020-10-06 13:07:17 -0400

    Discord add numbers as Number not String

--- discord/webview.js
+++ discord/webview.js
@@ -9,14 +9,13 @@ function _interopRequireDefault(obj) {
 module.exports = Franz => {
   const getMessages = function getMessages() {
     //prettier-ignore
-    const unreadDirectMsgs = Number(
+    const unreadDirectMsgs =
       Array
         .from(document.querySelectorAll('[class^="numberBadge"]'))
-        .reduce((acc, el) => acc + el.textContent, 0)
-    );
+        .reduce((acc, el) => acc + Number(el.textContent), 0);
 
     const unreadGuildConvos = document.querySelectorAll(
-      '[data-ref-id="guildsnav"] div[class^="pill"] span[class^="item"][style*="height: 8px"]'
+      '[data-ref-id="guildsnav"] div[class^="pill"] span[class^="item"][style*="height: 8px"], div[class*="isUnread"]'
     ).length;
 
     Franz.setBadge(unreadDirectMsgs, unreadGuildConvos);
